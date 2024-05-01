2205f1fde2e3bae426728085dee68113fd508267
Eh0802 <ehsoe0802@gmail.com>
2024-05-01 18:00:03 -0400

    Week 4 Project

---
+++ Pokedex.js
@@ -0,0 +1,112 @@
+
+const pokemons = document.getElementById("pokemons");
+
+const featchPokemon = (filter) => {
+
+    const promises = []
+    for (let i = 1; i <= 50; i++){
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    promises.push (fetch(url).then((response) =>  response.json()));
+    
+};
+
+    Promise.all(promises).then((results => {
+        const pokemons = results.map((data) => ({
+            name: data.name,
+            id: data.id,
+            height: data.height,
+            weight: data.weight,
+            image: data.sprites["front_default"],
+            type: data.types.map((type) => type.type.name). join(", ")
+            
+        }));
+
+        if (filter === 'LOW_TO_HIGH') {
+            const pokemonFilter = pokemons.sort((a, b) => a.id - b.id);
+        }
+        else if (filter === 'HIGH_TO_LOW') {
+            const pokemonFilter = pokemons.sort((a, b) => b.id - a.id);
+        }
+        else if (filter === 'A-Z') {
+            const pokemonFilter = pokemons.sort(function (a, b) {
+                if (a.name < b.name) {
+                    return -1; 
+                }
+                if (a.name > b.name) {
+                    return 1;
+                }
+                console.log(pokemonFilter)
+            })
+        }
+        else if (filter === 'Z-A') {
+            const pokemonFilter = pokemons.sort(function (a, b) {
+                if (a.name > b.name) {
+                    return -1; 
+                }
+                if (a.name < b.name) {
+                    return 1;
+                }
+                console.log(pokemonFilter)
+            })
+        }
+        displayPokemon(pokemons);
+    }));
+}
+
+
+const displayPokemon = (pokemon) => {
+
+    const pokeonHTMLSrtring = pokemon.map( card => `
+              <div class="pokemon">
+                <figure>
+                  <img
+                    src="${card.image}" 
+                    class="pokemon__img"
+                    alt="Pokemon picture"
+                  />
+                </figure>
+                <h3 class="pokemon__name">Name: <span class="name">${card.name}</span></h3>
+                <div class="id-type">
+                <h4 class="pokemon__id">ID: <span class="id">${card.id}</span></h4>
+                <h4 class="pokemon__type">Type: <span class="id">${card.type}</span></h4>
+                </div>
+                <div class="weigh__height">
+                    <h4 class="pokemon__weight">Weight: <span class="weight">${card.weight}</span></h4>
+                    <h4 class="pokemon__height">Height: <span class="height">${card.height}</span></h4>
+                </div>
+              </div>
+            </div>
+    `).join("");
+
+    pokemons.innerHTML = pokeonHTMLSrtring;
+    
+} 
+
+function filterPokemon(event) {
+    featchPokemon(event.target.value)
+}
+
+function openMenu() {
+    document.body.classList += " menu--open"
+    
+}
+function closeMenu() {
+    document.body.classList.remove('menu--open')
+}
+
+window.addEventListener("load", () => {
+    const loading = document.querySelector(".loading");
+
+    loading.classList.add("loader-hidden");
+
+    // loading.addEventListener("transitionend", () => {
+    //     document.body.removeChild("loading");
+    // })
+} )
+
+function searchPokemon() {
+    window.location.href = "http://127.0.0.1:5500/Search.html";
+}
+
+
+featchPokemon();
---
+++ Search.js
@@ -0,0 +1,61 @@
+
+
+const pokemonSprites = document.querySelector(" .pokemon__img")
+const pokemonName = document.querySelector(" .name")
+const pokemonId = document.querySelector(" .id")
+const pokemonType = document.querySelector(" .type")
+const pokemonWeight = document.querySelector(" .weight")
+const pokemonHeight = document.querySelector(" .height")
+
+searchPokemon();
+
+async function searchPokemon() {
+    try {
+        const namePokemon = document.getElementById("pokemonName").value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${namePokemon}`)
+        console.log(response)
+        if(!response.ok) {
+            throw new Error("Pokemon not found")
+        }
+        const data = await response.json();
+        console.log(data)
+        pokemonName.innerHTML = data.name;
+        pokemonId.innerHTML = data.id;
+        pokemonSprites.src = data.sprites['front_default'];
+        pokemonWeight.innerHTML = data.weight
+        pokemonHeight.innerHTML = data.height
+        pokemonType.innerHTML = data.types.map((type) => type.type.name)
+
+    }
+    catch(error){
+        console.error(error);
+    }
+
+    const seachLoading = document.querySelector(' .loading__search');
+            seachLoading.classList += " search__loading-complete"
+
+    setTimeout(() => {
+        seachLoading.classList.remove("search__loading-complete")
+    }, 1000);
+
+}
+
+function openMenu() {
+    document.body.classList += " menu--open"
+    
+}
+function closeMenu() {
+    document.body.classList.remove('menu--open')
+}
+
+window.addEventListener("load", () => {
+    const loading = document.querySelector(".loading");
+    loading.classList.add("loader-hidden");
+    loading.addEventListener("transitionend", () => {
+        document.body.removeChild("loading");
+    })
+} )
+
+function backToHomePage() {
+    window.location.href = "http://127.0.0.1:5500/PokeDex.html#";
+}
